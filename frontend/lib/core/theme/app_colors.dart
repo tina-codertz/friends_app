6701a215a41e3ba5c95adfab1fa99780496297eb
christina kimario <christinakimario8@gmail.com>
2026-05-27 21:58:44 +0300

    first commit

---
+++ frontend/lib/core/theme/app_colors.dart
@@ -0,0 +1,16 @@
+ //we define all colors to be used in the app globally in here to avoid code duplication and to make it easier to change the colors later
+import 'package:flutter/material.dart';
+
+class AppColors {
+  AppColors._();
+
+  static const Color background = Color(0xFF0A0A0F);
+  static const Color cardBackground = Color(0xFF1A1A2E);
+  static const Color cyan = Color(0xFF00F5FF);
+  static const Color pink = Color(0xFFE0B0FF);
+  static const Color purple = Color(0xFF7B2FBE);
+  static const Color purpleDark = Color(0xFF2D1B4E);
+  static const Color white = Color(0xFFFFFFFF);
+  static const Color grey = Color(0xFF888888);
+  static const Color inputBackground = Color(0xFF2A2A3E);
+}
---
+++ frontend/lib/core/theme/app_text_styles.dart
@@ -0,0 +1,56 @@
+//define all the text size weight color spacing in one place to avoid code duplication and to make it easier to change the text styles later
+
+import "package:flutter/material.dart";
+import "package:frontend/core/theme/app_colors.dart";
+
+class AppTextStyles{
+    AppTextStyles.();
+
+    static const TextStyle appTitle = TextStyle(
+        fontSize:32,
+        fontWeight:FontWeight.bold,
+        color:AppColors.pink,
+    );
+    static const TextStyle tagline = TextStyle(
+    fontSize:12,
+    fontWeight:FontWeight.w500,
+    color:AppColors.white,
+    letterSpacing:4,
+    );
+
+    statuc const TextStyle body = TextStyle(
+        fontSize:13,
+        fontWeight:FontWeight.normal,
+        color:AppColors.white,
+        height:1.5,
+    );
+    static const TextStyle label = TextStyle(
+        fontSize:12,
+        fontWeight:FontWeight.w500,
+        color:AppColors.white,
+    );
+    static const TextStyle link - TextStyle(
+        fontSize:14,
+        fontWeight:FontWeight.w500,
+        color:AppColors.cyan
+    );
+
+    statuc const TextStyle buttonPrimary = TextStyle(
+        fontSize:16,
+        fontWeight:FontWeight.w600,
+        color:AppColors.background,
+    );
+
+    static const TextStyle  buttonSecondary = TextStyle(
+        fontSize:14,
+        fontWeight:FontWeight.w500,
+        color:AppColors.white,
+    );
+
+    static const TextStyle cardTitle = TextStyle(
+        fontSize:16,
+        fontWeight:FontWeight.bold,
+        color:AppColors.white,
+    );
+
+}
